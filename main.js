e56fb5bef310cba3f5674f59fde7d1c3693404d9
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-09-24 17:42:12 -0400

    Add topbar menu

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
+// Create the main window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
-        width: 500,
+        width: isDev ? 1000 : 500,
         height: 600,
     });
+
+    // Open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+
+// App is ready
 app.whenReady().then(() => {
     createMainWindow();
+
+    // Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
 
     app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -22,6 +35,20 @@ app.whenReady().then(() => {
         }
     });
 });
+
+// Menu template
+const menu = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "Quit",
+                click: () => app.quit(),
+                accelerator: "CmdOrCtrl+W"
+            }
+        ]
+    }
+];
 
 app.on("window-all-closed", () => {
     if (!isMac) {
